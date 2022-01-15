993ee35d0c6ee5bdc5f89cac5c4dfcc078aaa496
om jha <jhaom1521@gmail.com>
2022-01-15 11:47:04 +0530

    Basic Info

--- methods/condition_if_else_explain.rb
+++ methods/condition_if_else_explain.rb
@@ -79,3 +79,83 @@ def authenticate_agent(rank,name,credentials)
 end
 
 authenticate_agent("008","Om Jha","Secret Agent")
+
+
+#nested if statement
+def meal_plan(time_of_week,time_of_day)
+  if time_of_week == "weekday"
+    if time_of_day == "morning"
+       "idli sambar"
+    elsif time_of_day == "afternoon"
+         "rice daal sabzi"
+    elsif time_of_day == "night"
+         "roti bhujiya"
+    end
+  elsif time_of_week == "weekend"
+    if time_of_day == "morning"
+       "roti sabzi"
+    elsif time_of_day == "afternoon"
+         "chicken rice"
+    elsif time_of_day == "night"
+       "sattu roti"
+    end
+  end
+  end
+
+p meal_plan("weekday","afternoon")
+
+#respond_to? method
+num = 999
+p num.respond_to?("next") #respond_to checks - a way to check if method is applicable or not
+p num.respond_to?(:next) #just another way
+if num.respond_to?("next")
+  p num.next
+end
+
+#ternary operator
+puts 1 < 2 ? "that's true" : "that's false"
+num = 1201
+puts num.even? ? "Yes.. Its even" : "No. Its odd"
+
+
+#default and optional argument
+def make_phone_call(number,international_code = 1,area_code = 646)
+  puts "Dialled number is #{international_code}-#{area_code}-#{number}"
+end
+
+make_phone_call(9905512345)
+make_phone_call(9905512345,4)
+make_phone_call(9905512345,4,121)
+
+#call method from method
+def add(a,b)
+  a+b
+end
+
+def substract(a,b)
+  a-b
+end
+
+def multiply(a,b)
+  a*b
+end
+
+def divide(a,b)
+  a/b
+end
+
+def calculator(a,b,operation="add")
+  if operation == "add"
+    add(a,b)
+  elsif operation == "substract"
+    substract(a,b)
+elsif operation == "multiply"
+  multiply(a,b)
+elsif operation == "divide"
+  divide(a,b)
+  else
+    "That's not a valid operation"
+  end
+end
+
+p calculator(10,11,"substract")
