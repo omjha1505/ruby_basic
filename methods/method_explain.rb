8397c61f80c7364242795430f3b117b8851a727e
om jha <jhaom1521@gmail.com>
2022-01-15 10:05:28 +0530

    Basic Info

---
+++ methods/method_explain.rb
@@ -0,0 +1,17 @@
+#Method declaration with no argument
+def my_introduction
+  expression = "Ruby is fun" #Local Variable declaration
+  puts "I am Om Prakash Jha"
+  puts "I am 28"
+  puts expression
+end
+
+#Parameterized method
+def praise_person(name,age)
+  puts "#{name} is amazing person. He's just #{age}"
+end
+
+#invoking Method
+#my_introduction
+#my_introduction()
+#praise_person("Om Prakash Jha",26)
