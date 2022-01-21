662f57c80e1d498418010a554766a9f8f582cc8f
om jha <jhaom1521@gmail.com>
2022-01-21 19:22:18 +0530

    Basic Info

--- blocs_procs_lambda/blocks_expalin.rb
+++ blocs_procs_lambda/blocks_expalin.rb
@@ -3,3 +3,54 @@
   *It must be attached to a method call
   *can be defined with {} or do end
 =end
+
+[2,4,6,8,10].each {|elm| puts elm.even?}
+
+colors = ["red","black","white"]
+statements = colors.each {|color| "#{color} is great color"}
+p statements
+
+5.times {|index| puts "Iteration Number #{index} is running"}
+
+#yield keyword
+
+def pass_control
+  puts "This is inside pass_control method"
+  yield #pass control from method to block
+  puts "I am back in pass_control method"
+end
+
+pass_control{puts "I am Yield Block"}
+
+pass_control do
+  puts "This is line one of Yield"
+  puts "This is line two of Yield"
+end
+
+
+def who_am_i
+  adjective = yield
+  puts "I am very #{adjective}"
+end
+who_am_i{"stong"}
+who_am_i{"Talented"}
+
+
+def multiple_pass
+  puts "I am in multiple_pass"
+  yield
+  puts "I am back in multiple_pass"
+  yield
+end
+
+p multiple_pass{"I am repetative yield"} #return of yield is getting returned here
+
+
+def multiple_pass_second_version
+  puts "I am in multiple_pass second version"
+  yield
+  puts "I am back in multiple_pass second version"
+  yield
+end
+
+p multiple_pass_second_version{puts "I am repetative yield"} #this will be nil as puts return the nil
---
+++ blocs_procs_lambda/procs_explain.rb
@@ -0,0 +1,107 @@
+=begin
+Procs is new type of Object
+very efficient way of writing the repetative blocks
+=end
+
+cubes = Proc.new { |num| num **3 }
+
+a = [1,2,3,4]
+b = [5,6,7,8]
+c=  [9,10,11,12]
+
+a_cubes = a.map(&cubes)
+b_cubes = b.map(&cubes)
+c_cubes = c.map(&cubes)
+
+p a_cubes,b_cubes,c_cubes
+
+#more efficent way
+
+a_cubes_2 , b_cubes_2 , c_cubes_3 = [a,b,c].map { |array| array.map(&cubes) }
+p a_cubes_2 , b_cubes_2 , c_cubes_3
+
+currencies = [10,20,30,40]
+to_euros = Proc.new { |currency| currency * 0.95 }
+to_rupees = Proc.new { |currency| currency * 68.13 }
+
+p currencies.map(&to_euros)
+p currencies.map(&to_rupees)
+
+ages = [10,30,65,72]
+
+is_old = Proc.new do |age|
+  age > 55
+end
+
+p ages.select(&is_old)
+p ages.reject(&is_old)
+
+#.block_given method
+def pass_control_on_condition
+  puts "Inside the Method"
+  yield if block_given?
+  puts "Back Inside the method"
+end
+pass_control_on_condition
+pass_control_on_condition{"Hi..I am Yield"}
+
+def speak_the_truth
+  yield "OmJha" if block_given?
+end
+
+speak_the_truth{ |name| puts "#{name} is lolll" }
+speak_the_truth{ |name| puts "#{name} is awsome" }
+
+
+def speak_the_truth(name)
+  yield name if block_given?
+end
+speak_the_truth("OmJha"){ |name| puts "#{name} is very rude" }
+
+
+
+def speak_the_truth(name,age)
+  yield name,age if block_given?
+end
+speak_the_truth("OmJha",26) do |name , age|
+  p name,age
+  puts "#{name} is #{age} year old"
+end
+
+
+def number_evaluation(num1,num2,num3)
+  puts "inside the number_evaluation method"
+  yield num1,num2,num3
+  puts "once again inside the number_evaluation method"
+end
+
+number_evaluation(12,14,15) do |num1,num2,num3|
+  p num1,num2,num3
+  puts "The sum of #{num1},#{num2},#{num3} is #{num1+num2+num3}"
+end
+
+#custom each method using yield
+ def custom_each(array)
+   i = 0
+   while i < array.length
+     yield array[i]
+     i += 1
+   end
+ end
+
+ names = ["Om","Boo","Moo"]
+ numbers = [2,34,134536]
+
+ custom_each(names){|name| puts "content is #{name}"}
+ custom_each(numbers){|num| puts "Number is #{num}"}
+
+# calling function with the parameter even if parameter is not defined in function
+def greeter
+  puts "I'm in greetor method"
+end
+
+phrase = Proc.new do
+  puts "Insie the Proc"
+end
+
+greeter(&phrase)
